e7a6f6d3fa4992a8dd576496be13bc735bd8772b
liaimv <liaimv0301@gmail.com>
2026-01-31 11:26:37 -0500

    Mask Changing Code

--- Assets/1_Mask Game/Scripts/MaskChanger.cs
+++ Assets/1_Mask Game/Scripts/MaskChanger.cs
@@ -1,16 +1,49 @@
+using System.Collections;
 using UnityEngine;
 
 public class MaskChanger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //Masks
+    public SpriteRenderer maskRenderer;
+    public Sprite redMask;
+    public Sprite yellowMask;
+    public Sprite greenMask;
+    public Sprite blueMask;
+    public Sprite whiteMask;
+
+    private Sprite[] masks;
+
+    public Animator animator;
+
+    public float minSpeed = 1f;
+    public float maxSpeed = 10f;
+
+    void Awake()
     {
-        
+        masks = new Sprite[] { redMask, yellowMask, greenMask, blueMask, whiteMask };
+        ShowRandomMask();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ShowRandomMask()
     {
-        
+        int index = Random.Range(0, masks.Length);
+        maskRenderer.sprite = masks[index];
+    }
+
+    public void ChangeMask()
+    {
+        if (Random.value > 0.2f)
+        {
+            int newIndex;
+            do
+            {
+                newIndex = Random.Range(0, masks.Length);
+            } while (masks[newIndex] == maskRenderer.sprite);
+
+            maskRenderer.sprite = masks[newIndex];
+
+            float newSpeed = Random.Range(minSpeed, maxSpeed);
+            animator.speed = newSpeed;
+        }
     }
 }
